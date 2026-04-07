cd7ae82dfb5eea0e19d4f729f4dacbca37937bb9
Gkrab228 <kr0988720390@gmail.com>
2026-04-07 23:20:09 +0200

    ADDED: interactive items; FIXED: responsive design problems

--- Website/script.js
+++ Website/script.js
@@ -7,22 +7,49 @@ document.querySelector('.burger-menu').addEventListener('click', () => {
 
 const cans = document.querySelectorAll(".parralax");
 
-parralaxEffect()
+
 document.addEventListener("scroll", parralaxEffect); 
+parralaxEffect();
+
 
 function parralaxEffect(){
 
     for(let can of cans){
         const coords = can.getBoundingClientRect();
-        let scrollableDistance = window.innerHeight + coords.height;
+        const scrollableDistance = window.innerHeight + coords.height;
 
         if(coords.top<scrollableDistance){    
-            const amp = 300;
+            const amp = 600;
             const normalizedValue = ((window.innerHeight - coords.top) / scrollableDistance);
-            const finalValue =  (normalizedValue * amp) - 100;
+            const finalValue =  (normalizedValue * amp) - 300;
             
-            can.style.marginBottom = finalValue + "px";
+           can.style.marginBottom = `${Math.floor(finalValue)}px`
         }
     }
 
+};
+
+interactiveItems(document.querySelector(".presentation-section"), [
+    {item: cans[0], amp: -10, rotate: 2},
+    {item:  document.querySelector(".h-Green"), amp: 10},
+]);
+
+
+
+function interactiveItems(parent,items){
+    parent.addEventListener("mousemove",(e)=>{
+        
+        const valueX = (e.clientX/parent.clientWidth) - 0.5;
+        const valueY = (e.clientY/parent.clientHeight) - 0.5;
+        
+        items.forEach(element => {
+            let finalValue = `translate(${valueX*element.amp}px,${valueY*element.amp}px)`;
+            
+            if(element?.rotate&&window.innerWidth>700){
+                const normValue = -Math.sqrt(valueX*valueX + valueY *valueY);
+            }
+            
+            element.item.style.transform = finalValue;
+        });
+    })
 };
