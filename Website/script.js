280280a155320fe5d4886fe77e3541223d383ef4
Gkrab228 <kr0988720390@gmail.com>
2026-04-11 09:29:35 +0200

    FIXED: interactive items functionality, part of responsive CSS

--- Website/script.js
+++ Website/script.js
@@ -30,26 +30,57 @@ function parralaxEffect(){
 };
 
 interactiveItems(document.querySelector(".presentation-section"), [
-    {item: cans[0], amp: -10, rotate: 2},
+    {item: cans[0], amp: -5, rotate: 2,scale: -0.01},
     {item:  document.querySelector(".h-Green"), amp: 10},
 ]);
 
 
-
 function interactiveItems(parent,items){
+    if(!parent||document.documentElement.clientWidth<700) return;
+    console.log()
     parent.addEventListener("mousemove",(e)=>{
         
-        const valueX = (e.clientX/parent.clientWidth) - 0.5;
-        const valueY = (e.clientY/parent.clientHeight) - 0.5;
-        
         items.forEach(element => {
-            let finalValue = `translate(${valueX*element.amp}px,${valueY*element.amp}px)`;
-            
-            if(element?.rotate&&window.innerWidth>700){
-                const normValue = -Math.sqrt(valueX*valueX + valueY *valueY);
+            if(document.documentElement.clientWidth<700){
+                element.item.style.transform = "";
+                return false;
+                
             }
+            let coords  = element.item.getBoundingClientRect();
+            const center = getCentersOfElement(coords);
+
+            const valueX =  e.pageX/center.x;
+            const valueY = e.pageY/center.y;
             
+            let finalValue = `translate(${valueX*element.amp}px,${valueY*element.amp}px)`;
+
+            if(element.rotate){
+                const distance = getDistance(coords,e,center);
+                finalValue += `rotate(${distance*element.rotate}deg)`;
+            }
+            if(element.scale){
+                const mult = getDistance(coords,e,center)*element.scale +  1;
+                finalValue += `scale(${mult})`;
+            }
             element.item.style.transform = finalValue;
         });
+
+
     })
+    function getDistance(coords,event,center){
+        
+        const vectorX = center.x - event.clientX;
+        const vectorY = center.y - event.clientY;
+        
+        const distance = Math.sqrt(vectorX*vectorX + vectorY*vectorY);
+        const distanceCenter = Math.sqrt(center.y*center.y + center.x*center.x)
+        return distance/distanceCenter;
+    }
+
+    function getCentersOfElement(coords){
+        let center = {
+            x: coords.right - (coords.width/2) + window.scrollX,
+            y: coords.bottom-(coords.height/2) + window.scrollY};
+        return center;
+    }
 };
