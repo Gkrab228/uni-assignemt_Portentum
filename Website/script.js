391b870f89a48c9a4dc9af8b12756bec775ef402
Gkrab228 <kr0988720390@gmail.com>
2026-04-14 13:04:49 +0200

    ADDED: contact us subpage and visual features to it

--- Website/script.js
+++ Website/script.js
@@ -1,19 +1,12 @@
- 
 
-document.querySelector('.burger-menu').addEventListener('click', () => {
-    const navMenu = document.querySelector('nav');
-    navMenu.classList.toggle('open');
-});
 
 const cans = document.querySelectorAll(".parralax");
-
 
 document.addEventListener("scroll", parralaxEffect); 
 parralaxEffect();
 
 
 function parralaxEffect(){
-
     for(let can of cans){
         const coords = can.getBoundingClientRect();
         const scrollableDistance = window.innerHeight + coords.height;
@@ -34,28 +27,28 @@ interactiveItems(document.querySelector(".presentation-section"), [
     {item:  document.querySelector(".h-Green"), amp: 10},
 ]);
 
+interactiveItems(document.querySelector(".banner-section"), [
+    {item: cans[1], amp: -5, rotate: 2,scale: -0.01},
+    {item: document.querySelector(".h-Taste"), amp: 10},
+]);
+
 
 function interactiveItems(parent,items){
-    if(!parent||document.documentElement.clientWidth<700) return;
-    console.log()
+    if(!parent||detectMob()) return;
+
     parent.addEventListener("mousemove",(e)=>{
-        
         items.forEach(element => {
-            if(document.documentElement.clientWidth<700){
-                element.item.style.transform = "";
-                return false;
-                
-            }
+
             let coords  = element.item.getBoundingClientRect();
             const center = getCentersOfElement(coords);
 
             const valueX =  e.pageX/center.x;
             const valueY = e.pageY/center.y;
-            
+ 
             let finalValue = `translate(${valueX*element.amp}px,${valueY*element.amp}px)`;
 
             if(element.rotate){
-                const distance = getDistance(coords,e,center);
+                const distance = getDistance(e,center);
                 finalValue += `rotate(${distance*element.rotate}deg)`;
             }
             if(element.scale){
@@ -67,20 +60,97 @@ function interactiveItems(parent,items){
 
 
     })
-    function getDistance(coords,event,center){
-        
+    function getDistance(event,center){
         const vectorX = center.x - event.clientX;
         const vectorY = center.y - event.clientY;
-        
+
         const distance = Math.sqrt(vectorX*vectorX + vectorY*vectorY);
         const distanceCenter = Math.sqrt(center.y*center.y + center.x*center.x)
+
         return distance/distanceCenter;
     }
 
-    function getCentersOfElement(coords){
-        let center = {
-            x: coords.right - (coords.width/2) + window.scrollX,
-            y: coords.bottom-(coords.height/2) + window.scrollY};
-        return center;
-    }
+
 };
+
+function getCentersOfElement(coords){
+    let center = {
+        x: window.scrollX + coords.right - (coords.width/2),
+        y: window.scrollY + coords.top + (coords.height/2)};
+    return center;
+}
+
+
+
+
+const backgroundBin = document.querySelector(".background-bin");
+
+if(backgroundBin)  backgroundGenerate();
+
+function backgroundGenerate(){
+    const sizeEl = document.createElement("div");
+    sizeEl.innerHTML = "1";
+    sizeEl.style.width = "min-content";
+    backgroundBin.append(sizeEl);
+    const charHeight = parseInt(getComputedStyle(sizeEl).height);
+    const charWidth = sizeEl.clientWidth;
+    sizeEl.remove();
+
+    const charVert = backgroundBin.parentNode.clientHeight / charHeight;
+    const charHoriz = backgroundBin.parentNode.clientWidth / charWidth;
+    let resultDivs =  getListDivs(charVert,charHoriz);
+
+    backgroundBin.append(...resultDivs)
+
+    if(!detectMob()){
+        document.addEventListener("mousemove", setCursorStyle);
+
+        function setCursorStyle(event){
+            if(event.target.tagName!="SPAN") return;
+            event.target.style.color = "var(--Primary-Lime)";
+            setTimeout(() => {
+                event.target.style.color = "";
+            }, 100);
+        }
+    }
+    
+}
+
+function getListDivs(charVert,charHoriz){
+    let result = [];
+    for(let i=0;i<charVert;i++){
+        let div = document.createElement("div");
+        for(let j=0;j<charHoriz;j++){
+        const span = document.createElement("span");
+        span.innerHTML = Math.round(Math.random());
+        div.append(span);
+        }
+        result.push(div);
+    }
+    setInterval(()=>{randomSpanInsertion(result)},500);
+    return result;
+}
+
+function randomSpanInsertion(result){
+    const randomDiv = Math.round(result.length*Math.random());
+    const span = document.createElement("span");
+    span.innerHTML = "0"+ Math.round(Math.random());
+    result[randomDiv].style.color = "rgb(45, 45, 45)";
+    result[randomDiv].prepend(span);
+    setTimeout(() => {
+        result[randomDiv].style.color = "";
+    }, 100);
+    
+}
+
+
+
+document.querySelector('.burger-menu').addEventListener('click', () => {
+    const navMenu = document.querySelector('nav');
+    navMenu.classList.toggle('open');
+});
+
+
+function detectMob() {
+    return ( ( window.innerWidth <= 800 ) );
+}
